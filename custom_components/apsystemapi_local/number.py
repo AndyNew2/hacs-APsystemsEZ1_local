e2161f33251a328cfb748331d25be755dc529b75
Andy <145973686+AndyNew2@users.noreply.github.com>
2026-04-10 12:22:52 +0200

    set maxNumber to unavailable

--- custom_components/apsystemapi_local/number.py
+++ custom_components/apsystemapi_local/number.py
@@ -130,6 +130,11 @@ class ApSystemsDefaultMaxOutputNumber(CoordinatorEntity, ApSystemsEntity, Number
         self._attr_native_max_value = data.coordinator.api.max_power
         self._attr_native_min_value = data.coordinator.api.min_power
 
+    @property
+    def available(self) -> bool:
+        """Return if the entity is available."""
+        return self._attr_available
+
     async def async_update(self) -> None:
         """Set the state with the value fetched from the inverter."""
 
@@ -141,13 +146,9 @@ class ApSystemsDefaultMaxOutputNumber(CoordinatorEntity, ApSystemsEntity, Number
         except:
             _LOGGER.debug("Exception update default max power. Retry next cycle...")
             self._attr_available = False
-            # self._attr_state = None # self._attr_state = STATE_UNAVAILABLE # self.async_write_ha_state()
-            # for an unknown reason, we cannot set the entity to unavailable anymore. Either HA is broken or something is wrong here, any help welcome...
         else:
-            # self._attr_state = STATE_OK
             self._attr_native_value = status
-            self._attr_available = False
-            # self.async_write_ha_state()
+            self._attr_available = True
 
     async def async_set_native_value(self, value: float) -> None:
         """Set the desired output power."""
